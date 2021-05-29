e6056a5295f664951dbee469c7d3161a9e561b76
Phophon Insee <75802904+Phophon-Insee@users.noreply.github.com>
2021-05-30 06:42:21 +0700

    Add files via upload

---
+++ Urine.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import model
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
+
+@app.post("/upload_image/")
+async def upload_image(file: UploadFile = File(...)):
+    result = ""
+    with open("saved_image/"+file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        class_name, percent = model.recognizaion(file.filename)
+    if class_name == 0:
+        result = "White"
+        print(class_name)
+    elif class_name == 1:
+        print(class_name)
+        result = "yellow"
+    elif class_name == 2:
+        print(class_name)
+        result = "dark yellow"
+    return {"percent": percent,
+            "result": result}
---
+++ model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import PIL
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+new_model = tf.keras.models.load_model('saved_model/my_model')
+def recognizaion(name):
+    img = keras.preprocessing.image.load_img('saved_image/'+name, target_size=(180, 180))
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    predictions = new_model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    return np.argmax(score), 100 * np.max(score)
