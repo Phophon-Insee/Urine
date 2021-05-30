2a65510894a1544649be49e823c1bc4182058beb
Phophon Insee <75802904+Phophon-Insee@users.noreply.github.com>
2021-05-30 17:21:29 +0700

    Update Urine.py

--- app/Urine.py
+++ app/Urine.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, UploadFile, File
 import shutil
 import app.model as m
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.com",
+    "http://localhost",
+    "http://localhost:80",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/index")
 async def root():
